e0972d6721d77029217fab1f808bc1b0d33d6644
flyingfish16 <phong.duongnam@gmail.com>
2023-06-21 02:25:29 +0900

    Re-write resize as a function

--- editor/resize.py
+++ editor/resize.py
@@ -1,19 +1,24 @@
 import cv2
 import numpy as np
- 
-cap = cv2.VideoCapture('C:/New folder/video.avi')
- 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 5, (1280,720))
- 
-while True:
-    ret, frame = cap.read()
-    if ret == True:
-        b = cv2.resize(frame,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
-        out.write(b)
-    else:
-        break
-    
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+
+
+def resize(in_video, out_video, height, width):
+    assert out_video.endswith(".avi"), "Output must end with .avi"
+    cap = cv2.VideoCapture(in_video)
+
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(out_video, fourcc, 5, (height, width))
+
+    while True:
+        ret, frame = cap.read()
+        if ret is True:
+            b = cv2.resize(
+                frame, (height, width), fx=0, fy=0, interpolation=cv2.INTER_CUBIC
+            )
+            out.write(b)
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
