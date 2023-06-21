b96d11267552c496ac69b0468426e9f291204308
flyingfish16 <phong.duongnam@gmail.com>
2023-06-22 00:32:54 +0900

    modify the video2gif def

--- convertor/video2gif.py
+++ convertor/video2gif.py
@@ -2,38 +2,29 @@ from moviepy.editor import VideoFileClip
 import argparse
 from utils import *
 
-video_ext = ['avi', 'mp4']
-gif_ext = ['gif']
+video_ext = ["avi", "mp4"]
+gif_ext = ["gif"]
 
-def video2gif(args):
-    assert args.fps is not None, "You must set fps in arguments"
-    assert args.height is not None, "You must set height in arguments, 0 for not resize"
 
-    if args.input.split(".")[-1] not in video_ext:
+def video2gif(in_vid, out_vid, fps, height):
+    assert fps is not None, "You must set fps in arguments"
+    assert height is not None, "You must set height in arguments, 0 for not resize"
+
+    if in_vid.split(".")[-1] not in video_ext:
         print(f"Input video file path error. Video file must end with {video_ext}")
         exit()
-    if args.output.split(".")[-1] not in gif_ext:
+    if out_vid.split(".")[-1] not in gif_ext:
         print(f"Output gif file path error. Gif file must end with {video_ext}")
         exit()
-    if args.fps is not None and args.fps <= 0:
+    if fps is not None and fps <= 0:
         print(f"FPS must greater than 0")
         exit()
 
-    videoClip = VideoFileClip(args.input)
+    videoClip = VideoFileClip(in_vid)
 
-    if args.height != 0:
-        videoClip = videoClip.resize(height=args.height)
+    if height != 0:
+        videoClip = videoClip.resize(height=height)
 
-    videoClip.write_gif(args.output, fps=args.fps)
+    videoClip.write_gif(out_vid, fps=fps)
 
-    print(f"Export gif file completed. Gif is saved in {args.output}")
-
-
-if __name__ == "__main__":
-    parser = basic_parser()
-    parser.add_argument("--fps", type=int, default=None)
-    parser.add_argument("--height", type=int, default=None)
-    args = parser.parse_args()
-
-    video2gif(args)
-
+    print(f"Export gif file completed. Gif is saved in {out_vid}")
